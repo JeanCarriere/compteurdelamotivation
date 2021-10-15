dde0f33db28087d4c47c97be9c30b1c810320307
esunyer <54360051+esunyer@users.noreply.github.com>
2021-10-15 20:03:55 +0200

    Mini refactoring and fix of minor formating issues

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import engine from './engine.png';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      capacite:'',
-      controle:'',
-      valeur:'',
-      securisant: '',
-      encourageant: '',
+    this.state = {
+      capacite: '',
+      controle: '',
+      valeur: '',
       bienveillant: '',
+      encourageant: '',
+      securisant: '',
       stimulant: ''
     };
   }
 
   check = (val, max) => {
-    if(/^\+?\d+$/.test(val)) {
-      if(Number(val) > max) {
+    if (/^\+?\d+$/.test(val)) {
+      if (Number(val) > max) {
         return max + '';
       }
       return val;
@@ -31,11 +31,11 @@ class App extends React.Component {
     const {capacite,
     controle,
     valeur,
-    securisant,
-    encourageant,
     bienveillant,
+    encourageant,
+    securisant,
     stimulant} = this.state;
-    const score = Math.floor(100*((Number(capacite) + Number(controle)) * Number(valeur) + Number(securisant) + Number(encourageant) + Number(bienveillant) + Number(stimulant)) / 200)
+    const score = Math.floor(100 * ((Number(capacite) + Number(controle)) * Number(valeur) + Number(bienveillant) + Number(encourageant) + Number(securisant) + Number(stimulant)) / 200)
   return (
     <div className="wrapper">
       <h1>Compteur de la motivation</h1>
@@ -44,7 +44,7 @@ class App extends React.Component {
         <div className="app__left">
           <div className="app__box">
             <h2>Compétence</h2>
-            <div className="input"><span>Capacité</span> <input type="text" value={capacite} onChange={(e) => this.setState({capacite: this.check(e.target.value, 5)})}/><span> / 5</span></div>
+            <div className="input"><span>Capacité</span><input type="text" value={capacite} onChange={(e) => this.setState({capacite: this.check(e.target.value, 5)})}/><span> / 5</span></div>
             <div className="input"><span>Contrôle</span><input type="text" value={controle} onChange={(e) => this.setState({controle: this.check(e.target.value, 5)})}/><span> / 5</span></div>
           </div>
           <div className="app__box">
@@ -56,16 +56,16 @@ class App extends React.Component {
             <div className="input"><span>Bienveillant</span><input type="text" value={bienveillant} onChange={(e) => this.setState({bienveillant: this.check(e.target.value, 25)})}/><span> / 25</span></div>
             <div className="input"><span>Encourageant</span><input type="text" value={encourageant} onChange={(e) => this.setState({encourageant: this.check(e.target.value, 25)})}/><span> / 25</span></div>
             <div className="input"><span>Sécurisant</span><input type="text" value={securisant} onChange={(e) => this.setState({securisant: this.check(e.target.value, 25)})}/><span> / 25</span></div>
-            <div className="input"><span>Stimulant</span><input type="text"  value={stimulant} onChange={(e) => this.setState({stimulant: this.check(e.target.value, 25)})}/><span> / 25</span></div>
+            <div className="input"><span>Stimulant</span><input type="text" value={stimulant} onChange={(e) => this.setState({stimulant: this.check(e.target.value, 25)})}/><span> / 25</span></div>
           </div>
         </div>
         <div className="app__right">
-          <GaugeChart className="gauge" id="gauge-chart2" textColor={'#fff'} nrOfLevels={10} percent={score/100} colors={["#FF0000","#00FF00"]} />
+          <GaugeChart className="gauge" id="gauge-chart2" textColor={'#FFF'} nrOfLevels={10} percent={score / 100} colors={['#FF0000', '#00FF00']} />
           <div className="app__box last">
             <h2>Ma motivation</h2>
             <h3>Compétence : {Number(capacite) + Number(controle)} / 10</h3>
             <h3>Valeur : {valeur} / 10</h3>
-            <h3>Environnement : {Number(securisant) + Number(encourageant) + Number(bienveillant) + Number(stimulant)} / 100</h3>
+            <h3>Environnement : {Number(bienveillant) + Number(encourageant) + Number(securisant) + Number(stimulant)} / 100</h3>
             <h3>Score : {score} %</h3>
           </div>
         </div>
