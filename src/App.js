46143ea8fb4514a445db2e39c10d26e958837c2e
esunyer <54360051+esunyer@users.noreply.github.com>
2021-10-15 19:19:29 +0200

    Modify display of ratios and percentage

--- src/App.js
+++ src/App.js
@@ -43,30 +43,30 @@ class App extends React.Component {
       <div className="app">
         <div className="app__left">
           <div className="app__box">
-            <h2>Compétence /10</h2>
-            <div className="input"><span>Capacité</span> <input type="text" value={capacite} onChange={(e) => this.setState({capacite: this.check(e.target.value, 5)})}/><span>/5</span></div>
-            <div className="input"><span>Contrôle</span><input type="text" value={controle} onChange={(e) => this.setState({controle: this.check(e.target.value, 5)})}/><span>/5</span></div>
+            <h2>Compétence</h2>
+            <div className="input"><span>Capacité</span> <input type="text" value={capacite} onChange={(e) => this.setState({capacite: this.check(e.target.value, 5)})}/><span> / 5</span></div>
+            <div className="input"><span>Contrôle</span><input type="text" value={controle} onChange={(e) => this.setState({controle: this.check(e.target.value, 5)})}/><span> / 5</span></div>
           </div>
           <div className="app__box">
-            <h2>Valeur /10</h2>
-            <div className="input"><span>Valeur</span><input type="text" value={valeur} onChange={(e) => this.setState({valeur: this.check(e.target.value, 10)})}/><span>/10</span></div>
+            <h2>Valeur</h2>
+            <div className="input"><span>Valeur</span><input type="text" value={valeur} onChange={(e) => this.setState({valeur: this.check(e.target.value, 10)})}/><span> / 10</span></div>
           </div>
           <div className="app__box">
-            <h2>Environnement /100</h2>
-            <div className="input"><span>Sécurisant</span><input type="text" value={securisant} onChange={(e) => this.setState({securisant: this.check(e.target.value, 25)})}/><span>/25</span></div>
-            <div className="input"><span>Encourageant</span><input type="text" value={encourageant} onChange={(e) => this.setState({encourageant: this.check(e.target.value, 25)})}/><span>/25</span></div>
-            <div className="input"><span>Bienveillant</span><input type="text" value={bienveillant} onChange={(e) => this.setState({bienveillant: this.check(e.target.value, 25)})}/><span>/25</span></div>
-            <div className="input"><span>Stimulant</span><input type="text"  value={stimulant} onChange={(e) => this.setState({stimulant: this.check(e.target.value, 25)})}/><span>/25</span></div>
+            <h2>Environnement</h2>
+            <div className="input"><span>Sécurisant</span><input type="text" value={securisant} onChange={(e) => this.setState({securisant: this.check(e.target.value, 25)})}/><span> / 25</span></div>
+            <div className="input"><span>Encourageant</span><input type="text" value={encourageant} onChange={(e) => this.setState({encourageant: this.check(e.target.value, 25)})}/><span> / 25</span></div>
+            <div className="input"><span>Bienveillant</span><input type="text" value={bienveillant} onChange={(e) => this.setState({bienveillant: this.check(e.target.value, 25)})}/><span> / 25</span></div>
+            <div className="input"><span>Stimulant</span><input type="text"  value={stimulant} onChange={(e) => this.setState({stimulant: this.check(e.target.value, 25)})}/><span> / 25</span></div>
           </div>
         </div>
         <div className="app__right">
           <GaugeChart className="gauge" id="gauge-chart2" textColor={'#fff'} nrOfLevels={10} percent={result/100} colors={["#FF0000","#00FF00"]} />
           <div className="app__box last">
             <h2>Ma motivation</h2>
-            <h3>Compétence: {Number(capacite) + Number(controle)}</h3>
-            <h3>Valeur: {valeur}</h3>
-            <h3>Environnement: {Number(securisant) + Number(encourageant) + Number(bienveillant) + Number(stimulant)}</h3>
-            <h3>Résultat: {result}%</h3>
+            <h3>Compétence: {Number(capacite) + Number(controle)} / 10</h3>
+            <h3>Valeur: {valeur} / 10</h3>
+            <h3>Environnement: {Number(securisant) + Number(encourageant) + Number(bienveillant) + Number(stimulant)} / 100</h3>
+            <h3>Résultat: {result} %</h3>
           </div>
         </div>
       </div>
