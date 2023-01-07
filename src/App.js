e37181128fbc5251ee94002b8f560aa0463e8846
Jean Carriere <jcarriere@webgeoservices.com>
2023-01-07 20:24:45 +0100

    improvements

--- src/App.js
+++ src/App.js
@@ -37,10 +37,6 @@ class App extends React.Component {
     stimulant} = this.state;
     const score = Math.floor(100 * ((Number(capacite) * Number(valeur) + Number(controle))  + Number(bienveillant) + Number(encourageant) + Number(securisant) + Number(stimulant)) / 200)
     const date = new Date();
-
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
     return (
     <div className="wrapper">
       <h1>Compteur de la motivation</h1>
@@ -48,28 +44,25 @@ class App extends React.Component {
       <div className="app">
         <div className="app__left">
           <div className="app__box">
-            <h2>Compétence</h2>
             <div className="input"><span>Compétence</span><input type="number" value={capacite} onChange={(e) => this.setState({capacite: this.check(e.target.value, 10)})}/><span>&nbsp;/&nbsp;10</span></div>
           </div>
           <div className="app__box">
-            <h2>Valeur</h2>
-            <div className="input"><span>Valeur</span><input type="number" value={valeur} onChange={(e) => this.setState({valeur: this.check(e.target.value, 10)})}/><span> / 10</span></div>
+            <div className="input"><span>Valeur</span><input style={{width:'60px'}} type="number" value={valeur} onChange={(e) => this.setState({valeur: this.check(e.target.value, 10)})}/><span> / 10</span></div>
           </div>
           <div className="app__box">
-            <h2>Contrôle</h2>
-            <div className="input"><span>Contrôle</span><input type="number" value={controle} onChange={(e) => this.setState({controle: this.check(e.target.value, 40)})}/><span> / 40</span></div>
+            <div className="input"><span>Contrôle</span><input style={{width:'60px'}} type="number" value={controle} onChange={(e) => this.setState({controle: this.check(e.target.value, 40)})}/><span> / 40</span></div>
           </div>
           <div className="app__box">
             <h2>Environnement</h2>
             <div className="input"><span>Bienveillant</span><input type="number" value={bienveillant} onChange={(e) => this.setState({bienveillant: this.check(e.target.value, 15)})}/><span>&nbsp;/&nbsp;15</span></div>
             <div className="input"><span>Encourageant</span><input type="number" value={encourageant} onChange={(e) => this.setState({encourageant: this.check(e.target.value, 15)})}/><span>&nbsp;/&nbsp;15</span></div>
-            <div className="input"><span>Sécurisant</span><input type="number" value={securisant} onChange={(e) => this.setState({securisant: this.check(e.target.value, 15)})}/><span>&nbsp;/&nbsp;15</span></div>
+            <div className="input"><span>Soutenant</span><input type="number" value={securisant} onChange={(e) => this.setState({securisant: this.check(e.target.value, 15)})}/><span>&nbsp;/&nbsp;15</span></div>
             <div className="input"><span>Stimulant</span><input type="number" value={stimulant} onChange={(e) => this.setState({stimulant: this.check(e.target.value, 15)})}/><span>&nbsp;/&nbsp;15</span></div>
           </div>
         </div>
         <div className="app__right">
           <GaugeChart className="gauge" id="gauge-chart2" textColor={'#FFF'} nrOfLevels={10} percent={score / 100} colors={['#FF0000', '#00FF00']} />
-          <div className="app__box last">
+          <div className="app__box">
             <h2>Ma motivation</h2>
             <h3>Compétence : {Number(capacite)} / 10</h3>
             <h3>Valeur : {Number(valeur)} / 10</h3>
@@ -79,9 +72,14 @@ class App extends React.Component {
           </div>
           <div className="app__box">
             <h2>Date</h2>
-            <div className="input">{day} / {month} / {year}</div>
+            <div className="input">{date.toLocaleDateString("fr")}</div>
+          </div>
+          <div className="app__box">
+            <h2>Prénom</h2>
+            <div className="input"><input type="text" /></div>
           </div>
         </div>
+
       </div>
     </div>
   );
